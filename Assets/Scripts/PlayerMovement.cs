cdefbe8c434e7c2beae43bcaa8f0a873f164caec
Muammar81 <Muammar.Yacoob@gmail.com>
2022-03-12 00:19:05 +0000

    Screen Boundaries Set

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,10 +1,15 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] InputActionReference leftStick;
+    //[SerializeField] InputActionReference fireButton;
     private float moveSpeed = 5f;
+
+    //private void OnEnable() => fireButton.action.performed += OnFire;
+    //private void OnDisable() => fireButton.action.performed -= OnFire;
 
     public void Update()
     {
@@ -17,7 +22,9 @@ public class PlayerMovement : MonoBehaviour
         if (direction.sqrMagnitude < 0.01)
             return;
 
-        var move =  new Vector3(direction.x, direction.y, 0);
+        var move =  new Vector3(- direction.x, direction.y, 0);
         transform.position += move * moveSpeed * Time.deltaTime;
     }
+
+    //private void OnFire(InputAction.CallbackContext ctx) => print($"fire{ctx.action.name}");
 }
